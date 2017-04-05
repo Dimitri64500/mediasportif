e0dcf4a0fe925519043f6dd58ac383b1aeb3793b
zimed <ziiimed@gmail.com>
2017-04-05 16:21:48 +0200

    un caroussel + logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { MdGridListModule, MdToolbarModule, MdButtonModule } from '@angular/material';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports:      [ BrowserModule, MdGridListModule, MdToolbarModule, MdButtonModule ],
+  imports:      [ BrowserModule, MdGridListModule, MdToolbarModule, MdButtonModule, NgbModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
