f832cc661ed3ac2a7203114b487a7a16fcef14d4
RhitaRah <rahni.rhita@gmail.com>
2017-04-05 14:28:07 +0200

    add modules + base project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import { MdGridListModule, MdToolbarModule, MdButtonModule } from '@angular/material';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, MdGridListModule, MdToolbarModule, MdButtonModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
