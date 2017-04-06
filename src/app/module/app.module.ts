7b7143a5266e853944a3e2ff52d03eec4dc30f10
RhitaRah <rahni.rhita@gmail.com>
2017-04-06 16:43:30 +0200

    amelioration css Timeline

--- src/app/module/app.module.ts
+++ src/app/module/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from '../component/app.component';
 import { MdGridListModule, MdToolbarModule, MdButtonModule } from '@angular/material';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {TimelineComponent} from '../component/timeline.component';
 
 
 
 @NgModule({
-  imports:      [ BrowserModule, MdGridListModule, MdToolbarModule, MdButtonModule, NgbModule],
+  imports:      [ BrowserModule, MdGridListModule, MdToolbarModule, MdButtonModule],
   declarations: [ AppComponent, TimelineComponent ],
   bootstrap:    [ AppComponent ]
 })
