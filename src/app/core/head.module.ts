e3d8339a390b055e4ada00e92541bcab6cf8fb86
corentin <co.rngt@sfr.fr>
2017-04-06 10:17:31 +0200

    Ajout d'un icon "test"

--- src/app/core/head.module.ts
+++ src/app/core/head.module.ts
@@ -4,10 +4,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeadComponent } from './head.component';
-import { MdGridListModule, MdToolbarModule, MdButtonModule } from '@angular/material';
+import { MdGridListModule, MdToolbarModule, MdButtonModule, MdIconModule } from '@angular/material';
 
 @NgModule({
-  imports:      [ BrowserModule, MdGridListModule, MdToolbarModule, MdButtonModule ],
+  imports:      [ BrowserModule, MdGridListModule, MdToolbarModule, MdButtonModule, MdIconModule ],
   declarations: [ HeadComponent ],
   bootstrap:    [ HeadComponent ]
 })
