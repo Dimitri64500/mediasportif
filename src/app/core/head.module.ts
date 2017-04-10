eb8b2720c456d585ecef5dceac75dd6f3489dede
corentin <co.rngt@sfr.fr>
2017-04-10 14:04:40 +0200

    Ajout des icons "home", "login", barre recherche

--- src/app/core/head.module.ts
+++ src/app/core/head.module.ts
@@ -4,12 +4,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeadComponent } from './head.component';
-import { AppComponent }   from './../app.component';
 import { MdGridListModule, MdToolbarModule, MdButtonModule, MdIconModule } from '@angular/material';
 
 @NgModule({
   imports:      [ BrowserModule, MdGridListModule, MdToolbarModule, MdButtonModule, MdIconModule ],
-  declarations: [ HeadComponent, AppComponent ],
-  bootstrap:    [ HeadComponent, AppComponent ]
+  declarations: [ HeadComponent ],
+  bootstrap:    [ HeadComponent ]
 })
 export class HeadModule { }
