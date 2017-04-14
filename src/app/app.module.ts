e693295d42732dfa44a5a2ad7bebed6e0d9f752b
rajoyc <camille.rajoy@etu.u-bordeaux.fr>
2017-04-14 11:39:26 +0200

    Ajout de la partie admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,34 +14,24 @@ import {CarouselComponent} from './Site/Carousel/carousel.component';
 import {RecapArticleComponent} from './Admin/RecapArticles/recap-article.component';
 import {AjoutProgrammeComponent} from './Admin/AjoutProgramme/ajout-programme.component';
 import {AjoutArticleComponent} from './Admin/AjoutArticle/ajout-article.component';
-
 import {TwitterComponent} from './Site/Twitter/twitter.component';
 import {LiensComponent} from './Site/Liens/liens.component';
 
 @NgModule({
   imports: [BrowserModule, MdGridListModule, MdToolbarModule, MdButtonModule, HttpModule, AppRoutingModule],
-  declarations: [AppComponent, TimelineComponent, LoginComponent, AdminAccueilComponent, AccueilComponent, MenuComponent, RecapArticleComponent, AjoutProgrammeComponent, AjoutArticleComponent],
-  providers: [AuthenticationService],
-  bootstrap: [AppComponent]
-  imports:      [ BrowserModule, MdGridListModule, MdToolbarModule, MdButtonModule, HttpModule, AppRoutingModule],
-  declarations: [ AppComponent, TimelineComponent, LoginComponent, AdminAccueilComponent, AccueilComponent, MenuComponent, CarouselComponent ],
-  imports:      [ BrowserModule,
-    MdGridListModule,
-    MdToolbarModule,
-    MdButtonModule,
-    MdIconModule,
-    HttpModule,
-    AppRoutingModule],
-  declarations: [ AppComponent,
+  declarations: [AppComponent,
     TimelineComponent,
     LoginComponent,
     AdminAccueilComponent,
     AccueilComponent,
     MenuComponent,
+    RecapArticleComponent,
+    AjoutProgrammeComponent,
+    AjoutArticleComponent,
     CarouselComponent,
     TwitterComponent,
     LiensComponent],
-  providers:    [ AuthenticationService],
-  bootstrap:    [ AppComponent ]
+  providers: [AuthenticationService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
