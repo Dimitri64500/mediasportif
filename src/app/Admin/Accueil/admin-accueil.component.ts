10e12b99ae08142b0345e20d97255bf2d37563a6
rajoyc <camille.rajoy@etu.u-bordeaux.fr>
2017-04-14 11:33:19 +0200

    Ajout de la partie admin

---
+++ src/app/Admin/Accueil/admin-accueil.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-admin-accueil',
+  templateUrl: './admin-accueil.component.html',
+  styleUrls: ['./admin-accueil.component.scss', ]
+})
+
+export class AdminAccueilComponent { }
