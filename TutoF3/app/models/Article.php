4d3146728262aa13eedd409c08c943e6acd65261
corentin <co.rngt@sfr.fr>
2017-04-26 10:33:42 +0200

    Timeline - ajout restriction sur les brouillons, archives

--- TutoF3/app/models/Article.php
+++ TutoF3/app/models/Article.php
@@ -12,7 +12,7 @@ class Article extends Model
         return $this->query;
     }
     public function allArticlesAndNote(){
-      $result = $this->db->exec('(SELECT note.titre as titre, note.texte as texte,note.datetime as date, null as url FROM `note`) UNION ALL( SELECT article.titre as titre, article.resume as texte, article.date as date, article.url as url FROM article) ORDER BY date DESC');
+      $result = $this->db->exec('(SELECT note.titre as titre, note.texte as texte,note.datetime as date, null as url FROM `note`) UNION ALL( SELECT article.titre as titre, article.resume as texte, article.date as date, article.url as url FROM article where article.status=\'active\') ORDER BY date DESC');
       return $result;
     }
 
