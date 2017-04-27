a405e66def32118377a19e6794c5027cb3cb028c
Dimitri <dimitri64500@hotmail.fr>
2017-04-27 11:35:09 +0200

    Ajout du modal Login

--- TutoF3/app/controllers/MainController.php
+++ TutoF3/app/controllers/MainController.php
@@ -142,11 +142,21 @@ class MainController extends Controller
     echo json_encode($json);
   }
 
-  function upload()
-  {
-    $uploads_dir = '/uploads';
-    $tmp_name = $_FILES['file_contents']['tmp_name'];
-    $name = $_FILES['file_contents']['name'];
-    move_uploaded_file($tmp_name, "$uploads_dir/$name");
+  // API qui va insérer une image à la une dans le dossier "uploads"
+  // Renvoi le nom de l'image insérée
+  function upload(){
+    $uploads_dir = getcwd().'/uploads';
+    $extensions = array('.png', '.gif', '.jpg', '.jpeg');
+    $tmp_name = $_FILES['myfile']['tmp_name'];
+    $name = $_FILES['myfile']['name'];
+    $extension = strrchr($_FILES['myfile']['name'], '.');
+    $taille = filesize($_FILES['myfile']['tmp_name']);
+    $name = preg_replace('/([^.a-z0-9]+)/i', '-', $name);
+    if(in_array($extension, $extensions) && $taille<2000000){
+      move_uploaded_file($tmp_name, "$uploads_dir/$name");
+      echo $name;
+    }
+    else
+      echo "fail";
   }
 }
