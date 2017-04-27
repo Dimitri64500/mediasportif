1a610cf35f75f0925027d9e977a4d58c0c4eeedd
corentin <co.rngt@sfr.fr>
2017-04-27 10:14:11 +0200

    accueil

--- src/app/Site/Carousel/carousel.component.ts
+++ src/app/Site/Carousel/carousel.component.ts
@@ -12,9 +12,10 @@ export class CarouselComponent {
 
   ngOnInit() {
     this.images = [];
-    this.images.push({source:'app/images/i1.jpg', alt:'Description for Image 1', title:'Title 1'});
-    this.images.push({source:'app/images/i2.jpg', alt:'Description for Image 2', title:'Title 2'});
-    this.images.push({source:'app/images/i3.jpg', alt:'Description for Image 3', title:'Title 3'});
-    this.images.push({source:'app/images/i4.jpg', alt:'Description for Image 4', title:'Title 4'});
+    this.images.push({source: 'app/images/i1.jpg', alt: 'Description for Image 1', title: 'Title 1'});
+    this.images.push({source: 'app/images/i2.jpg', alt: 'Description for Image 2', title: 'Title 2'});
+    this.images.push({source: 'app/images/i3.jpg', alt: 'Description for Image 3', title: 'Title 3'});
+    this.images.push({source: 'app/images/i4.jpg', alt: 'Description for Image 4', title: 'Title 4'});
  }
 }
+
--- src/app/Site/Twitter/twitter.component.ts
+++ src/app/Site/Twitter/twitter.component.ts
@@ -10,7 +10,18 @@ export class TwitterComponent implements AfterViewInit {
   constructor() {
   }
 
-  ngAfterViewInit() {
-
+  ngAfterViewInit () {
+    !function(d,s,id){
+      var js: any,
+        fjs=d.getElementsByTagName(s)[0],
+        p='https';
+      if(!d.getElementById(id)){
+        js=d.createElement(s);
+        js.id=id;
+        js.src=p+"://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js,fjs);
+      }
+    }
+    (document,"script","twitter-wjs");
   }
 }
